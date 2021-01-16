81f2d5de6f894663f533676c4136add2b3dad88d
dianaonit <72396521+dianaonit@users.noreply.github.com>
2021-01-16 16:00:12 +0200

    Add project files.

---
+++ Onit_Diana_Lab12/Onit_Diana_Lab12/Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Onit_Diana_Lab12.Models
+{
+    class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
